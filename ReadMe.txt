2514d2b25ce57da042228f1bb707221063d5c420
837033 <837033@SAIT225134IT.ACDM.DS.SAIT.CA>
2020-09-17 11:15:39 -0600

    gt

---
+++ ReadMe.txt
@@ -0,0 +1,13 @@
+Project Title : Movie Explorer
+
+This program is that Users can add or find information about movies in movie files. 
+Also, users are able to check the movie information in the movie file at random as the number of movies they want.
+
+The date : May 18, 2020
+
+The author : Sangmean Hong, Aiden(Gookjin) Kim
+
+Users can run this program by running a file with a "jar" extension on the command prompt. 
+Users should type "java -jar filename.jar" on the command prompt.
+
+
---
+++ src/sait/mms/problemdomain/Movie.java
@@ -0,0 +1,61 @@
+package sait.mms.problemdomain;
+
+/**
+The program reads in three values and calls various functions to 
+manipulate them.
+
+@author Hong and Kim
+@version 1.0, May 17, 2020
+*/
+public class Movie {
+	/**
+	 * This is three private instance field.
+	 */
+	private int year; //Year of Movie published.
+	private String movieName; // Name of Movie.
+	private int duration; // Movie's duration.
+
+	/**
+	 * This is a Movie constructor.
+	 * 
+	 * @param duration - Get duration parameter from MovieMangementSystem class
+	 * @param movieName - Get movieName parameter from MovieMangementSystem class
+	 * @param year - Get year parameter from MovieMangementSystem class
+	 */
+	public Movie(int duration, String movieName, int year) {
+		this.year = year;
+		this.movieName = movieName;
+		this.duration = duration;
+	}
+
+	// *******Accessor Methods**********
+	/**
+	 * The getYear method returns a Movie object's year.
+	 * 
+	 * @return year - Returns movie's year.
+	 */
+	public int getYear() {
+		return year;
+	}
+	/**
+	 * The getDuration method returns a Movie object's duration.
+	 * 
+	 * @return duration - Returns movie's duration.
+	 */
+	public int getDuration() {
+		return duration;
+	}
+
+	@Override 
+	/**
+	 * The toString method returns local variable str.
+	 * 
+	 * @return str - Returns local variable str.
+	 */
+	public String toString() {
+		String str = "";
+		str += String.format("%-12d%-6d%s%n", duration, year, movieName);
+		return str;
+	}
+
+}
